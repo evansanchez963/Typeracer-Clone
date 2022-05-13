023d15fd11f0309e15253be5f2772cfad61e3ed8
Frost274 <sancheev@sonoma.edu>
2022-05-13 13:04:22 -0700

    Style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,13 @@
+import logo from "../images/logo.png"
+
 const Navbar = () => {
   return (
     <nav id="navbar">
       <div id="links">
-        <p><strong>typeracer</strong></p>
+        <div id="logo">
+          <img src={logo} alt="logo"></img>
+          <p><strong>typeracer</strong></p>
+        </div>
         <p>Discord</p>
         <p>About</p>
       </div>
