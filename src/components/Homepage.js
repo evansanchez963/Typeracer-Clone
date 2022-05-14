bf1b261616b4086afac6d991803718a5ee03d8b1
Frost274 <sancheev@sonoma.edu>
2022-05-13 22:34:56 -0700

    Style home page and add game mode sections

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,35 @@
+import startLine from "../images/start-line.png"
+import typing from "../images/typing.png"
+
 const Homepage = () => {
   return (
     <section id="homepage">
-    
+
+      <div id="text">
+        <h1>typeracer - the Global Typing Competition</h1>
+        <p>Increase your typing speed while racing against others.</p>
+      </div>
+
+      <div id="game-modes">
+        <div id="race-online">
+          <div id="online-text">
+            <p>Get matched up with online opponents</p>
+            <button>Enter a typing race</button>
+          </div>
+
+          <img id="start-line" src={startLine} alt="start line"></img>
+        </div>
+
+        <div id="race-alone">
+          <div id="alone-text">
+            <p>Improve your typing skills on your own</p>
+            <button>Practice Yourself</button>
+          </div>
+
+          <img id="typing" src={typing} alt="typing"></img>
+        </div>
+      </div>
+
     </section>
   )
 }
