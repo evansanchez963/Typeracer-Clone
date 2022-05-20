8c08f7ac8c1c23ec3e2470f85c4c30ce0d4b017e
Frost274 <sancheev@sonoma.edu>
2022-05-19 21:49:34 -0700

    Condense states into objects and a custom hook

--- src/components/pages/PracticeYourself/PracticeYourself.js
+++ src/components/pages/PracticeYourself/PracticeYourself.js
@@ -5,43 +5,32 @@ import { useCalcWPM } from "./hooks/index"
 import "./PracticeYourself.css"
 
 const PracticeYourself = () => {
-  const [isLoading, setIsLoading] = useState(true)
-  const [error, setError] = useState(null)
-  const [chars, setChars] = useState(0)
-  const [words, setWords] = useState([])
-  const [currInput, setCurrInput] = useState("")
-  const [inputValid, setInputValid] = useState(true)
-  const [currCharIdx, setCurrCharIdx] = useState(-1)
-  const [currWordIdx, setCurrWordIdx] = useState(0)
-  const [isStarted, setIsStarted] = useState(false)
-  const [isEnded, setIsEnded] = useState(false)
-  const [countdown, setCountdown] = useState(3000)
-  const [countdownOn, setCountdownOn] = useState(false)
-  const [gameTimer, setGameTimer] = useState(60000)
-  const [gameTimerOn, setGameTimerOn] = useState(false)
-  const [charsTyped, setCharsTyped] = useState(0)
-  const [errors, setErrors] = useState(0)
-  const WPM = useCalcWPM(gameTimer, charsTyped, errors)
-  const [finalWPM, setFinalWPM] = useState(0)
-  const [time, setTime] = useState(0)
-  const [accuracy, setAccuracy] = useState(0.0)
+  const [loadInfo, setLoadInfo] = useState({isLoading: true, loadError: null})
+  const [textInfo, setTextInfo] = useState({chars: [], words: []})
+  const [inputInfo, setInputInfo] = useState({currInput: "", inputValid: true})
+  const [idxInfo, setIdxInfo] = useState({currCharIdx: -1, currWordIdx: 0})
+  const [gameStatus, setGameStatus] = useState({isStarted: false, isEnded: false})
+  const [countdown, setCountdown] = useState({time: 3000, on: false})
+  const [gameTimer, setGameTimer] = useState({time: 60000, on: false})
+  const [userTypeInfo, setUserTypeInfo] = useState({charsTyped: 0, errors: 0})
+  const [userStats, setUserStats] = useState({finalWPM: 0, time: 0, accuracy: 0})
+  const WPM = useCalcWPM(gameTimer.time, userTypeInfo.charsTyped, userTypeInfo.errors)
 
   useEffect(() => {
     const getData = async () => {
       try {
-        const url = "http://metaphorpsum.com/paragraphs/1/1"
-        const response = await fetch(url)
+        const response = await fetch("http://metaphorpsum.com/paragraphs/1/1")
         const text = await response.text()
-        setChars(text.split(""))
-        setWords(text.split(" "))
-        setIsLoading(false)
-        setCountdownOn(true)
+        setTextInfo(prev => ({...prev, chars: text.split("")}))
+        setTextInfo(prev => ({...prev, words: text.split(" ")}))
+        setLoadInfo(prev => ({...prev, isLoading: false}))
+        setCountdown(prev => ({...prev, on: true}))
         if(!response.ok) {
           throw Error(response.statusText)
         }
-      } catch(error) {
-        setError(error)
-        setIsLoading(false)
+      } catch(loadError) {
+        setLoadInfo(prev => ({...prev, loadError: loadError}))
+        setLoadInfo(prev => ({...prev, isLoading: false}))
       }
     }
 
@@ -51,78 +40,71 @@ const PracticeYourself = () => {
   useEffect(() => {
     let interval = null
 
-    if(countdown < 0) {
-      setCountdownOn(false)
-      setIsStarted(true)
-      setGameTimerOn(true)
-    } else if(countdownOn) {
+    if(countdown.time < 0) {
+      setCountdown(prev => ({...prev, on: false}))
+      setGameStatus(prev => ({...prev, isStarted: true}))
+      setGameTimer(prev => ({...prev, on: true}))
+    } else if(countdown.on) {
       interval = setInterval(() => {
-        setCountdown(prev => prev - 1000)
+        setCountdown(prev => ({...prev, time: prev.time - 1000}))
       }, 1000)
-    } else if (!countdownOn) {
+    } else if (!countdown.on) {
       clearInterval(interval)
     }
 
     return () => clearInterval(interval)
-  }, [countdown, countdownOn]) 
+  }, [countdown.time, countdown.on]) 
 
   useEffect(() => {
     let interval = null
 
-    if(isEnded) {
-      setGameTimerOn(false)
-    } else if(gameTimer < 0) {
-      setGameTimerOn(false)
-      setIsEnded(true)
-    } else if(gameTimerOn) {
+    if(gameStatus.isEnded) {
+      setGameTimer(prev => ({...prev, on: false}))
+    } else if(gameTimer.time < 0) {
+      setGameTimer(prev => ({...prev, on: false}))
+      setGameStatus(prev => ({...prev, isEnded: true}))
+    } else if(gameTimer.on) {
       interval = setInterval(() => {
-        setGameTimer(prev => prev - 1000)
+        setGameTimer(prev => ({...prev, time: prev.time - 1000}))
       }, 1000)
-    } else if(!gameTimerOn) {
+    } else if(!gameTimer.on) {
       clearInterval(interval)
     }
 
     return () => clearInterval(interval)
-  }, [gameTimer, gameTimerOn, isEnded])
-
-  /*
-  useEffect(() => {
-    if(wordsTyped === 0 || gameTimer === 60000) setWPM(0)
-    else setWPM(Math.floor(wordsTyped / (((60000 - gameTimer) / 1000) / 60)))
-  }, [gameTimer, wordsTyped])
-  */
+  }, [gameTimer.time, gameTimer.on, gameStatus.isEnded])
 
   useEffect(() => {
-    if(isEnded) {
-      setCurrInput("")
-      setFinalWPM(WPM)
-      setTime(60000 - gameTimer)
-      setAccuracy((((charsTyped - errors) / charsTyped) * 100).toFixed(1))
+    if(gameStatus.isEnded) {
+      setInputInfo(prev => ({...prev, currInput: ""}))
+      setUserStats(prev => ({...prev, finalWPM: WPM}))
+      setUserStats(prev => ({...prev, time: 60000 - gameTimer.time}))
+      setUserStats(prev => ({...prev, accuracy: (((userTypeInfo.charsTyped - userTypeInfo.errors) / userTypeInfo.charsTyped) * 100).toFixed(1)}))
     }
-  }, [isEnded, WPM, gameTimer, charsTyped, errors])
+  }, [gameStatus.isEnded, WPM, gameTimer.time, userTypeInfo.charsTyped, userTypeInfo.errors])
 
   const getWordClass = (wordIdx) => {
-    if(wordIdx === currWordIdx) return "active-word"
+    if(wordIdx === idxInfo.currWordIdx) return "active-word"
     return ""
   }
 
   const getCharClass = (char, charIdx, wordIdx) => {
-    if(charIdx === currCharIdx && wordIdx === currWordIdx) {
-      if(char === currInput.split("")[charIdx]) return "correct"
+    if(charIdx === idxInfo.currCharIdx && wordIdx === idxInfo.currWordIdx) {
+      if(char === inputInfo.currInput.split("")[charIdx]) return "correct"
       else {
         return "incorrect"
       }
     } 
     // Put blinking cursor on active character.
-    else if(charIdx === currCharIdx + 1 && wordIdx === currWordIdx) {
+    else if(charIdx === idxInfo.currCharIdx + 1 && wordIdx === idxInfo.currWordIdx) {
       return "active-char"
     }
     // Set past characters on current word as correct.
-    else if(wordIdx === currWordIdx && charIdx < currCharIdx) {
+    else if(wordIdx === idxInfo.currWordIdx && charIdx < idxInfo.currCharIdx) {
       return "correct"
     } 
     // Set all past words as correct.
-    else if(wordIdx < currWordIdx) {
+    else if(wordIdx < idxInfo.currWordIdx) {
       return "correct"
     }
 
@@ -135,56 +117,52 @@ const PracticeYourself = () => {
     
     if(event.key === " ") { // Space evaluates word at current word index.
 
-      if(currInput === words[currWordIdx]) {
-        setCurrInput("")
-        setCurrCharIdx(-1)
-        setCurrWordIdx(prev => prev + 1)
-        setCharsTyped(prev => prev + 1)
+      if(inputInfo.currInput === textInfo.words[idxInfo.currWordIdx]) {
+        setInputInfo(prev => ({...prev, currInput: ""}))
+        setIdxInfo(prev => ({...prev, currCharIdx: -1}))
+        setIdxInfo(prev => ({...prev, currWordIdx: prev.currWordIdx + 1}))
+        setUserTypeInfo(prev => ({...prev, charsTyped: prev.charsTyped + 1}))
       } else {
-        if(inputValid) {
-          setCurrInput(prev => prev + " ")
-          setCurrCharIdx(prev => prev + 1)
+        if(inputInfo.inputValid) {
+          setInputInfo(prev => ({...prev, currInput: prev.currInput + " "}))
+          setIdxInfo(prev => ({...prev, currCharIdx: prev.currCharIdx + 1}))
         }
-        setInputValid(false)
-        setErrors(prev => prev + 1)
+        setInputInfo(prev => ({...prev, inputValid: false}))
+        setUserTypeInfo(prev => ({...prev, errors: prev.errors + 1}))
       }
 
     } else if(event.key === "Backspace") {
 
-      if(!inputValid) setInputValid(true)
-      if(currInput !== "") setCurrCharIdx(prev => prev - 1)
+      if(!inputInfo.inputValid) setInputInfo(prev => ({...prev, inputValid: true})) 
+      if(inputInfo.currInput !== "") setIdxInfo(prev => ({...prev, currCharIdx: prev.currCharIdx - 1})) 
 
     } else { // Evaluate whether current typed character is correct.
 
-      if(event.key !== "Shift" && inputValid) {
-        const lastWord = words[words.length - 1]
+      if(event.key !== "Shift" && inputInfo.inputValid) {
+        const lastWord = textInfo.words[textInfo.words.length - 1]
         const lastWordChars = lastWord.split("")
         const lastChar = lastWordChars[lastWordChars.length - 1]
 
         // If user is on the very last character and it's correct,
         // clear input and end game. Else evaluate.
-        if((words.length - 1 === currWordIdx) && (lastWordChars.length - 1 === currCharIdx + 1) && (lastChar === event.key)) {
-
-          setCurrCharIdx(-1)
-          setCurrWordIdx(prev => prev + 1)
-          setIsEnded(true)
-
+        if((textInfo.words.length - 1 === idxInfo.currWordIdx) && (lastWordChars.length - 1 === idxInfo.currCharIdx + 1) && (lastChar === event.key)) {
+          setIdxInfo(prev => ({...prev, currCharIdx: -1}))
+          setIdxInfo(prev => ({...prev, currWordIdx: prev.currWordIdx + 1}))
+          setGameStatus(prev => ({...prev, isEnded: true}))
         } else {
-
-          const charToCheck = words[currWordIdx].split("")[currCharIdx + 1]
+          const charToCheck = textInfo.words[idxInfo.currWordIdx].split("")[idxInfo.currCharIdx + 1]
 
           if(event.key !== charToCheck) {
-            if(inputValid) {
-              setCurrInput(prev => prev + event.key)
-              setCurrCharIdx(prev => prev + 1)
+            if(inputInfo.inputValid) {
+              setInputInfo(prev => ({...prev, currInput: prev.currInput + event.key}))
+              setIdxInfo(prev => ({...prev, currCharIdx: prev.currCharIdx + 1}))
             }
-            setInputValid(false)
-            setErrors(prev => prev + 1)
+            setInputInfo(prev => ({...prev, inputValid: false}))
+            setUserTypeInfo(prev => ({...prev, errors: prev.errors + 1}))
           } else {
-            setCurrCharIdx(prev => prev + 1)
-            setCharsTyped(prev => prev + 1)
+            setIdxInfo(prev => ({...prev, currCharIdx: prev.currCharIdx + 1}))
+            setUserTypeInfo(prev => ({...prev, charsTyped: prev.charsTyped + 1}))
           }
-
         }
       }
 
@@ -192,12 +170,12 @@ const PracticeYourself = () => {
   }
 
   const handleChange = (event) => {
-    setCurrInput(event.target.value.trim())
+    setInputInfo(prev => ({...prev, currInput: event.target.value.trim()}))
   }
 
-  if(error) {
-    return <div>Error: {error.message}</div>
-  } else if(isLoading) {
+  if(loadInfo.loadError) {
+    return <div>Error: {loadInfo.loadError.message}</div>
+  } else if(loadInfo.isLoading) {
     return <div>Loading...</div>
   } else {
     return (
@@ -206,21 +184,21 @@ const PracticeYourself = () => {
         <h1>Practice Racetrack</h1>
 
         <TypingSection 
-          chars={chars}
-          words={words} 
-          currInput={currInput} 
-          inputValid={inputValid}
+          chars={textInfo.chars}
+          words={textInfo.words} 
+          currInput={inputInfo.currInput} 
+          inputValid={inputInfo.inputValid}
           getWordClass={getWordClass}
           getCharClass={getCharClass}
           handleKeyDown={handleKeyDown} 
           handleChange={handleChange}
-          isStarted={isStarted}
-          isEnded={isEnded}
-          countdown={countdown}
-          countdownOn={countdownOn}
-          gameTimer={gameTimer}
-          gameTimerOn={gameTimerOn}
-          charsTyped={charsTyped}
+          isStarted={gameStatus.isStarted}
+          isEnded={gameStatus.isEnded}
+          countdown={countdown.time}
+          countdownOn={countdown.on}
+          gameTimer={gameTimer.time}
+          gameTimerOn={gameTimer.on}
+          charsTyped={userTypeInfo.charsTyped}
           WPM={WPM}
         ></TypingSection>
 
@@ -228,10 +206,10 @@ const PracticeYourself = () => {
           <Link to="/">
             <button id="leave-practice">Main menu (leave practice)</button>
           </Link>
-          <button id="new-race" style={{display: isEnded ? "block":"none"}} onClick={() => window.location.reload()}>New race</button>
+          <button id="new-race" style={{display: gameStatus.isEnded ? "block":"none"}} onClick={() => window.location.reload()}>New race</button>
         </div>
 
-        <Statistics isEnded={isEnded} finalWPM={finalWPM} time={time} accuracy={accuracy}></Statistics>
+        <Statistics isEnded={gameStatus.isEnded} finalWPM={userStats.finalWPM} time={userStats.time} accuracy={userStats.accuracy}></Statistics>
 
       </section>
     )
--- src/components/pages/PracticeYourself/hooks/useCalcWPM.js
+++ src/components/pages/PracticeYourself/hooks/useCalcWPM.js
@@ -1,11 +1,11 @@
 import { useState, useEffect } from "react"
 
 const calcWPM = (gameTimer, charsTyped, errors) => {
-  if(charsTyped === 0 || gameTimer === 60000) return 0
-
   const time = (((60000 - gameTimer) / 1000) / 60)
   const grossWPM = Math.floor((charsTyped / 5) / time)
   const netWPM = Math.floor(grossWPM - (errors / time))
+
+  if(charsTyped === 0 || gameTimer === 60000 || netWPM < 0) return 0
 
   return netWPM
 }
