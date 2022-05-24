19b4982d9eee45988e6988042cc3ef4e2d2d8408
Frost274 <sancheev@sonoma.edu>
2022-05-23 23:58:53 -0700

    Extract input event functions into helpers

--- src/components/pages/PracticeYourself/PracticeYourself.js
+++ src/components/pages/PracticeYourself/PracticeYourself.js
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { GameStatusInfo, ProgressBar, Paragraph, Input, ButtonRow, Statistics } from "./index"
 import { useFetch, useTimers, useCalcWPM, useStats } from "./hooks/index"
-import { getCharClass, getWordClass, getTime, getAccuracy } from "./helpers/index"
+import { getCharClass, getWordClass, getTime, getAccuracy, handleKeyDown, handleChange } from "./helpers/index"
 import "./PracticeYourself.css"
 
 const PracticeYourself = () => {
@@ -14,69 +14,6 @@ const PracticeYourself = () => {
   const [userTypeInfo, setUserTypeInfo] = useState({charsTyped: 0, errors: 0})
   const WPM = useCalcWPM(gameTimer.time, userTypeInfo.charsTyped, userTypeInfo.errors)
   const userStats = useStats(getAccuracy, gameStatus, gameTimer, setInputInfo, userTypeInfo, WPM) 
-
-  // Evaluate input when user types.
-  // If input is wrong, user can only backspace.
-  const handleKeyDown = (event) => {
-    
-    if(event.key === " ") { // Space evaluates word at current word index.
-
-      if(inputInfo.currInput === textInfo.words[idxInfo.currWordIdx]) {
-        setInputInfo(prev => ({...prev, currInput: ""}))
-        setIdxInfo(prev => ({...prev, currCharIdx: -1}))
-        setIdxInfo(prev => ({...prev, currWordIdx: prev.currWordIdx + 1}))
-        setUserTypeInfo(prev => ({...prev, charsTyped: prev.charsTyped + 1}))
-      } else {
-        if(inputInfo.inputValid) {
-          setInputInfo(prev => ({...prev, currInput: prev.currInput + " "}))
-          setIdxInfo(prev => ({...prev, currCharIdx: prev.currCharIdx + 1}))
-        }
-        setInputInfo(prev => ({...prev, inputValid: false}))
-        setUserTypeInfo(prev => ({...prev, errors: prev.errors + 1}))
-      }
-
-    } else if(event.key === "Backspace") {
-
-      if(!inputInfo.inputValid) setInputInfo(prev => ({...prev, inputValid: true})) 
-      if(inputInfo.currInput !== "") setIdxInfo(prev => ({...prev, currCharIdx: prev.currCharIdx - 1})) 
-
-    } else { // Evaluate whether current typed character is correct.
-
-      if(event.key !== "Shift" && inputInfo.inputValid) {
-        const lastWord = textInfo.words[textInfo.words.length - 1]
-        const lastWordChars = lastWord.split("")
-        const lastChar = lastWordChars[lastWordChars.length - 1]
-
-        // If user is on the very last character and it's correct,
-        // clear input and end game. Else evaluate.
-        if((textInfo.words.length - 1 === idxInfo.currWordIdx) && (lastWordChars.length - 1 === idxInfo.currCharIdx + 1) && (lastChar === event.key)) {
-          setIdxInfo(prev => ({...prev, currCharIdx: -1}))
-          setIdxInfo(prev => ({...prev, currWordIdx: prev.currWordIdx + 1}))
-          setGameStatus(prev => ({...prev, isEnded: true}))
-        } else {
-          const charToCheck = textInfo.words[idxInfo.currWordIdx].split("")[idxInfo.currCharIdx + 1]
-
-          if(event.key !== charToCheck) {
-            if(inputInfo.inputValid) {
-              setInputInfo(prev => ({...prev, currInput: prev.currInput + event.key}))
-              setIdxInfo(prev => ({...prev, currCharIdx: prev.currCharIdx + 1}))
-            }
-            setInputInfo(prev => ({...prev, inputValid: false}))
-            setUserTypeInfo(prev => ({...prev, errors: prev.errors + 1}))
-          } else {
-            setIdxInfo(prev => ({...prev, currCharIdx: prev.currCharIdx + 1}))
-            setUserTypeInfo(prev => ({...prev, charsTyped: prev.charsTyped + 1}))
-          }
-
-        }
-      }
-
-    }
-  }
-
-  const handleChange = (event) => {
-    setInputInfo(prev => ({...prev, currInput: event.target.value.trim()}))
-  }
 
   const getStats = () => {
     if(gameStatus.isEnded) return <Statistics userStats={userStats} getTime={getTime}/> 
@@ -99,7 +36,10 @@ const PracticeYourself = () => {
 
           <div id="typing-box">
             <Paragraph words={textInfo.words} getCharClass={getCharClass} getWordClass={getWordClass} gameStatus={gameStatus} inputInfo={inputInfo} idxInfo={idxInfo}/>
-            <Input inputInfo={inputInfo} gameStatus={gameStatus} handleKeyDown={handleKeyDown} handleChange={handleChange}/>
+            <Input inputInfo={inputInfo} 
+            gameStatus={gameStatus} 
+            handleKeyDown={(event) => {handleKeyDown(event, setGameStatus, textInfo, inputInfo, setInputInfo, idxInfo, setIdxInfo, setUserTypeInfo)}} 
+            handleChange={(event) => {handleChange(event, setInputInfo)}}/>
           </div>
         </div>
 
--- src/components/pages/PracticeYourself/helpers/index.js
+++ src/components/pages/PracticeYourself/helpers/index.js
@@ -2,10 +2,14 @@ import getCharClass from "./getCharClass"
 import getWordClass from "./getWordClass"
 import getAccuracy from "./getAccuracy"
 import getTime from "./getTime"
+import handleKeyDown from "./handleKeyDown"
+import handleChange from "./handleChange"
 
 export {
   getCharClass, 
   getWordClass, 
   getAccuracy, 
-  getTime
+  getTime,
+  handleKeyDown,
+  handleChange
 }
