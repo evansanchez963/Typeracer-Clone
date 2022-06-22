2a60b63c56a5948551424e686763a6206ea85d52
Frost274 <sancheev@sonoma.edu>
2022-06-21 21:20:17 -0700

    Configre environment variables

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 
 require("dotenv").config({ path: "./config.env" })
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
