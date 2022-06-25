fca2815630d494cd3e74c0ddd4cc23c76977610f
Frost274 <sancheev@sonoma.edu>
2022-06-24 22:04:40 -0700

    Refactor models

--- server/models/user.js
+++ server/models/user.js
@@ -1,19 +1,33 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-  email: {type: String, required: true, maxlength: 50, unique: true},
-  user_name: {type: String, required: true, maxlength: 50, unique: true},
-  password: {type: String, required: true, maxlength: 50},
-  typing_sessions: [{type: Schema.Types.ObjectId, ref: 'Typing Session'}]
-})
-
-// Virtual for user's URL
-UserSchema
-.virtual('url')
-.get(() => {
-  return '/profile/user/' + this._id
+  email: {
+    type: String, 
+    required: [true, "Please provide a username."],
+    unique: true,
+    match: [
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+      "Please provide a valid email."
+    ]
+  },
+  user_name: {
+    type: String, 
+    required: [true, "Please provide a username."], 
+    unique: true
+  },
+  password: {
+    type: String, 
+    required: [true, "Please provide a password."],
+    select: false
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  typing_sessions: [{
+    type: Schema.Types.ObjectId, 
+    ref: "Typing Session"
+  }]
 })
 
 // Export model
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model("User", UserSchema)
