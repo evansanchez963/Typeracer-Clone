a5b04d8bcf2e4ea0cd115d54efbccd9fbfe8cc2c
Frost274 <sancheev@sonoma.edu>
2022-06-24 18:19:52 -0700

    Install bcrypt

--- server/controllers/createAccountController.js
+++ server/controllers/createAccountController.js
@@ -1,3 +1,34 @@
 const { body, validationResult } = require("express-validator")
 
 const User = require("../models/user")
+
+exports.createUser = [
+
+  // Validate and sanitize fields
+  body("ca-email").trim().exists().escape().withMessage("Email is required."),
+  body("ca-username").trim().exists().escape().withMessage("Username is required."),
+  body("ca-password").trim().exists.escape().withMessage("Password is required."),
+  body("ca-confirm-password").custom((value, { req }) => value === req.body.ca-pasword),
+
+  (req, res, next) => {
+
+    const errors = validationResult(req)
+    
+    if (!errors.isEmpty()) {
+      console.log(errors.array())
+      return;
+    }
+
+    const user = new User(
+      {
+        email: req.body.ca-email,
+        user_name: req.body.ca-username,
+        password: req.body.ca-password
+      }).save(err => {
+        if (err) {
+          return next(err)
+        }
+        res.redirect("/")
+      })
+  }
+]
