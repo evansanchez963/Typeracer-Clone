ae8ffedfb3ff1dfc59cd1c2e0a3b4b827e1f8fc3
Frost274 <sancheev@sonoma.edu>
2022-06-27 14:58:26 -0700

    Implement function for logging in

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -4,6 +4,7 @@ const User = require("../models/User")
 const bcrypt = require("bcryptjs")
 
 exports.createAccount = async (req, res, next) => {
+
   const {email, username, password} = req.body
 
   try {
@@ -37,15 +38,46 @@ exports.createAccount = async (req, res, next) => {
 }
 
 exports.login = async (req, res, next) => {
-  /*
+
   const { username, password } = req.body
 
   if(!username || !password) {
     res.status(400).json({ success: false, error: "Please provide a username and password." })
   }
-  */
 
-  res.send("Login Route")
+  try {
+    const database = client.db("Typeracer_Clone_Database")
+    const collection = database.collection("users")
+
+    const user = collection.findOne({ username }).select("+password")
+
+    if(!user) {
+      res.status(404).json({ 
+        success: false, 
+        error: "Invalid credentials"
+      })
+    }
+
+    const isMatch = bcrypt.compare(password, user.password)
+
+    if(!isMatch) {
+      res.status(404).json({
+        success: false,
+        error: "Invalid credentials"
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      user,
+    })
+  } catch(err) {
+    res.status(500).json({
+      success: false,
+      error: err.message
+    })
+  }
+  
 }
 
 exports.forgotPassword = (req, res, next) => {
