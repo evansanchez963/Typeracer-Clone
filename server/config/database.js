917526afd92f74619e0fa7e248212e9eeb8786f7
Frost274 <sancheev@sonoma.edu>
2022-06-27 14:00:25 -0700

    Refactor database connection file

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
-const connectDB = async () => {
-  await mongoose.connect(process.env.ATLAS_URI, {
+const connectDB = () => {
+  mongoose.connect(process.env.ATLAS_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
