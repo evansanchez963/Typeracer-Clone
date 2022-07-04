2d818781ded331aa23bf205d865781201472d8a4
Frost274 <sancheev@sonoma.edu>
2022-07-03 19:45:12 -0700

    Finish implementation for login page calling the backend for user login

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config({ path: "./config.env" })
 
 const express = require("express")
+const cors = require("cors")
 const connectDB = require("./config/database")
 const errorHandler = require("./middleware/error")
 
@@ -10,11 +11,15 @@ connectDB()
 const app = express()
 
 app.use(express.json())
+app.use(cors({ 
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST"] 
+}))
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/private", require("./routes/private"))
 app.use(errorHandler)
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
  
 const server = app.listen(port, () => console.log(`Server is running on port: ${port}`))
 
