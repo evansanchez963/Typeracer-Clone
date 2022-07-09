5c4f44c51f7259c151aa009d4512d189d3622910
Frost274 <sancheev@sonoma.edu>
2022-07-08 21:00:30 -0700

    Style user information section in user profile page

--- client/src/components/pages/UserProfile/UserProfile.js
+++ client/src/components/pages/UserProfile/UserProfile.js
@@ -8,7 +8,7 @@ import "./UserProfile.css"
 const UserProfile = ({ isLoggedIn }) => {
 
   const [avgWPM, setAvgWPM] = useState(0)
-  const [highestWPM, setHighestWMP] = useState(0)
+  const [highestWPM, setHighestWPM] = useState(0)
   const [raceCount, setRaceCount] = useState(0)
   const navigate = useNavigate()
 
@@ -22,10 +22,37 @@ const UserProfile = ({ isLoggedIn }) => {
       <div className="user-profile-container">
         <h1>Profile</h1>
 
-        <h2>Name</h2>
-        <h2>Email</h2>
+        <div className="user-personal-info">
+          <h2>1. Personal Information</h2>
+          <hr></hr>
+          <form id="user-info-form">
+            <div className="user-info-input">
+              <label htmlFor="user-info-username">Username:</label>
+              <input type="text" id="user-info-username" name="user-info-username"></input>
+            </div>
+            <div className="user-info-input">
+              <label htmlFor="user-info-email">Email:</label>
+              <input type="text" id="user-info-email" name="user-info-email"></input>
+            </div>
+            <input type="submit" value="Submit" id="user-info-submit"></input>
+          </form>
+        </div>
 
-        <button>Change Password</button>
+        <div className="user-change-password">
+          <h2>2. Change Password (optional)</h2>
+          <hr></hr>
+          <form id="user-password-form">
+            <div className="user-password-input">
+              <label htmlFor="user-password">Password:</label>
+              <input type="text" id="user-password" name="user-password"></input>
+            </div>
+            <div className="user-password-input">
+              <label htmlFor="user-confirm-password">Confirm Password:</label>
+              <input type="text" id="user-confirm-password" name="user-confirm-password"></input>
+            </div>
+            <input type="submit" value="Submit" id="user-password-submit"></input>
+          </form>
+        </div>
       </div>
 
       <div className="user-stats-container">
@@ -36,7 +63,7 @@ const UserProfile = ({ isLoggedIn }) => {
         <div className="user-stat-container">
           <BsFillKeyboardFill size={30}/>
           <div className="user-stat">
-            <p>Average WPM:&nbsp;</p>
+            <p>Avg. WPM:&nbsp;</p>
             <p>{avgWPM} WPM</p>
           </div>
         </div>
@@ -44,7 +71,7 @@ const UserProfile = ({ isLoggedIn }) => {
         <div className="user-stat-container">
           <SiSpeedtest size={30}/>
           <div className="user-stat">
-            <p>Highest WPM:&nbsp;</p>
+            <p>Best Race:&nbsp;</p>
             <p>{highestWPM} WPM</p>
           </div>
         </div>
