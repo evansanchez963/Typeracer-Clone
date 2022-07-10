ed48a4cda3a4dd6aa41d87af2f5593ba5a4a69be
Frost274 <sancheev@sonoma.edu>
2022-07-10 14:23:26 -0700

    Add comment to user route file

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const { getUserInfo } = require("../controllers/user")
 const { protect } = require("../middleware/protect")
 
+/* These are operations that only authenticated users can perform. */
+
 // Get private data from specific user.
 router.route("/:userId").get(protect, getUserInfo)
 
