6d3065038446941c6544eaa14c00d3055be762ab
Frost274 <sancheev@sonoma.edu>
2022-07-11 16:30:24 -0700

    Add progress and account deletion functions in DangerZone component

--- client/src/components/pages/UserProfile/DangerZone/DangerZone.js
+++ client/src/components/pages/UserProfile/DangerZone/DangerZone.js
@@ -1,11 +1,23 @@
 import "./DangerZone.css"
 
+const deleteProgressHandler = () => {
+  if(window.confirm("Are you sure you want to delete your progress?")) {
+    alert("Progress deleted!")
+  }
+}
+
+const deleteAccountHandler = () => {
+  if(window.confirm("Are you sure you want to delete your account?")) {
+    alert("Account deleted!")
+  }
+}
+
 const DangerZone = ({ isLoggedIn }) => {
   return (
     <div className="danger-zone-container">
       <h1>Danger Zone</h1>
-      <button className="danger-option">Delete Progress</button>
-      <button className="danger-option">Delete Account</button>
+      <button className="danger-option" onClick={deleteProgressHandler}>Delete Progress</button>
+      <button className="danger-option" onClick={deleteAccountHandler}>Delete Account</button>
     </div>
   )
 }
--- client/src/components/pages/UserProfile/UserInfo/UserInfo.js
+++ client/src/components/pages/UserProfile/UserInfo/UserInfo.js
@@ -41,42 +41,41 @@ const UserInfo = ({ isLoggedIn }) => {
   }
 
   return (
-    <>
-      <div className="user-profile-container">
-        <h1>Profile</h1>
-        <div className="user-personal-info">
-          <h2>1. Personal Information (update info here)</h2>
-          <hr></hr>
-          <form id="user-info-form" onSubmit={changeInfoHandler}>
-            <div className="user-info-input">
-              <label htmlFor="user-info-username">Username:</label>
-              <input type="text" id="user-info-username" name="user-info-username" onChange={(e) => setUsername(e.target.value)} value={username}></input>
-            </div>
-            <div className="user-info-input">
-              <label htmlFor="user-info-email">Email:</label>
-              <input type="text" id="user-info-email" name="user-info-email" onChange={(e) => setEmail(e.target.value)} value={email}></input>
-            </div>
-            <input type="submit" value="Submit" id="user-info-submit"></input>
-          </form>
-        </div>
+    <div className="user-profile-container">
+      <h1>Profile</h1>
 
-        <div className="user-change-password">
-          <h2>2. Change Password (optional)</h2>
-          <hr></hr>
-          <form id="user-password-form" onSubmit={changePasswordHandler}>
-            <div className="user-password-input">
-              <label htmlFor="user-password">Password:</label>
-              <input type="text" id="user-password" name="user-password"></input>
-            </div>
-            <div className="user-password-input">
-              <label htmlFor="user-confirm-password">Confirm Password:</label>
-              <input type="text" id="user-confirm-password" name="user-confirm-password"></input>
-            </div>
-            <input type="submit" value="Submit" id="user-password-submit"></input>
-          </form>
-        </div>
+      <div className="user-personal-info">
+        <h2>1. Personal Information (update info here)</h2>
+        <hr></hr>
+        <form id="user-info-form" onSubmit={changeInfoHandler}>
+          <div className="user-info-input">
+            <label htmlFor="user-info-username">Username:</label>
+            <input type="text" id="user-info-username" name="user-info-username" onChange={(e) => setUsername(e.target.value)} value={username}></input>
+          </div>
+          <div className="user-info-input">
+            <label htmlFor="user-info-email">Email:</label>
+            <input type="text" id="user-info-email" name="user-info-email" onChange={(e) => setEmail(e.target.value)} value={email}></input>
+          </div>
+          <input type="submit" value="Submit" id="user-info-submit"></input>
+        </form>
       </div>
-    </>
+
+      <div className="user-change-password">
+        <h2>2. Change Password (optional)</h2>
+        <hr></hr>
+        <form id="user-password-form" onSubmit={changePasswordHandler}>
+          <div className="user-password-input">
+            <label htmlFor="user-password">Password:</label>
+            <input type="text" id="user-password" name="user-password"></input>
+          </div>
+          <div className="user-password-input">
+            <label htmlFor="user-confirm-password">Confirm Password:</label>
+            <input type="text" id="user-confirm-password" name="user-confirm-password"></input>
+          </div>
+          <input type="submit" value="Submit" id="user-password-submit"></input>
+        </form>
+      </div>
+    </div>
   )
 }
 
