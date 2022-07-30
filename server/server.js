1a9a298ce5fe5826ca1e3ff85400740d00a8d0d2
Frost274 <sancheev@sonoma.edu>
2022-07-29 17:00:36 -0700

    Refactor socketio events

--- server/server.js
+++ server/server.js
@@ -32,13 +32,17 @@ io.on("connection", (socket) => {
   console.log(`User ${socket.id} connected`);
 
   socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User ${socket.id} connected to room ${data}`);
+    socket.join(data.room);
+    console.log(
+      `User ${data.user} with ID ${socket.id} connected to room ${data.room}`
+    );
   });
 
   socket.on("leave_room", (data) => {
-    socket.leave(data);
-    console.log(`User ${socket.id} left room ${data}`);
+    socket.leave(data.room);
+    console.log(
+      `User ${data.user} with ID ${socket.id} left room ${data.room}`
+    );
   });
 
   socket.on("disconnect", () => {
