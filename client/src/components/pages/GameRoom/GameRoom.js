dc6baa2837fa9e3fdebdd0030d7e335b12ea6935
Frost274 <sancheev@sonoma.edu>
2022-07-30 17:52:39 -0700

    Uninstall react-dotenv

--- client/src/components/pages/GameRoom/GameRoom.js
+++ client/src/components/pages/GameRoom/GameRoom.js
@@ -1,14 +1,31 @@
-import { useSocket } from "../../../context/SocketContext";
+import { useState, useEffect } from "react";
+//import { useSocket, useRoomCode } from "../../../context/SocketContext";
 import "./GameRoom.css";
 
 const GameRoom = () => {
+  const [joinedUsers, setJoinedUsers] = useState([]);
+  /*
   const socket = useSocket();
+  const room = useRoomCode();
 
-  //const [joinedUsers, setJoinedUsers] = useState([]);
+  const handleJoinedUsers = (data) => {
+    setJoinedUsers([...joinedUsers, data.user]);
+  };
+
+  useEffect(() => {}, [socket]);
+
+  const renderUsers = () => {
+    return joinedUsers.map((user, index) => (
+      <div key={user + index}>
+        <p>{user}</p>
+      </div>
+    ));
+  };
+  */
 
   return (
     <section id="game-room">
-      <h1>Connected Users: numUsers</h1>
+      <h1>Connected Users: {joinedUsers.length}</h1>
     </section>
   );
 };
