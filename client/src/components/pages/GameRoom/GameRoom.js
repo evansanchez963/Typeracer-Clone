d6d01fb32d4e1eaa8f9f8fca62381f9c000667ed
Frost274 <sancheev@sonoma.edu>
2022-07-31 19:22:44 -0700

    Correctly display connected users on client side in GameRoom component

--- client/src/components/pages/GameRoom/GameRoom.js
+++ client/src/components/pages/GameRoom/GameRoom.js
@@ -1,31 +1,47 @@
 import { useState, useEffect } from "react";
-//import { useSocket, useRoomCode } from "../../../context/SocketContext";
+import { useSocket, useRoomCode } from "../../../context/SocketContext";
 import "./GameRoom.css";
 
 const GameRoom = () => {
-  const [joinedUsers, setJoinedUsers] = useState([]);
-  /*
+  const [userRoster, setUserRoster] = useState({});
+
   const socket = useSocket();
-  const room = useRoomCode();
+  const roomCode = useRoomCode();
 
-  const handleJoinedUsers = (data) => {
-    setJoinedUsers([...joinedUsers, data.user]);
+  const updateJoinedUsers = (data) => {
+    setUserRoster({});
+    const userInfo = {};
+    for (const client in data) {
+      userInfo[client] = data[client];
+    }
+    setUserRoster(userInfo);
   };
-
-  useEffect(() => {}, [socket]);
 
   const renderUsers = () => {
-    return joinedUsers.map((user, index) => (
-      <div key={user + index}>
-        <p>{user}</p>
-      </div>
-    ));
+    return (
+      <ul>
+        {Object.keys(userRoster).map((client) => {
+          return (
+            <li key={client}>
+              {userRoster[client]} {socket.id === client ? "(you)" : ""}
+            </li>
+          );
+        })}
+      </ul>
+    );
   };
-  */
+
+  useEffect(() => {
+    socket.on("get_user_roster", updateJoinedUsers);
+    socket.emit("connect_to_room", { room: roomCode });
+
+    return () => socket.off("get_user_roster", updateJoinedUsers);
+  }, [socket, roomCode]);
 
   return (
     <section id="game-room">
-      <h1>Connected Users: {joinedUsers.length}</h1>
+      <h1>Connected Users: {Object.keys(userRoster).length}</h1>
+      {renderUsers()}
     </section>
   );
 };
