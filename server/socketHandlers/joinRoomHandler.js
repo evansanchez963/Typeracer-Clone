2976c3cc09488aa5eb7613d19513ae4e2057dd7c
Frost274 <sancheev@sonoma.edu>
2022-08-03 10:36:22 -0700

    Refactor joinRoomHandler function

--- server/socketHandlers/joinRoomHandler.js
+++ server/socketHandlers/joinRoomHandler.js
@@ -1,30 +1,25 @@
 const getActiveRooms = require("../utils/getActiveRooms");
 
-const joinRoom = (socket, connectedClients, data) => {
-  socket.join(data.room);
-  connectedClients[socket.id] = data.user;
-  socket.emit("join_room_success", { room: data.room });
-
-  console.log(
-    `User ${data.user} with ID ${socket.id} connected to room ${data.room}`
-  );
-};
-
 const joinRoomHandler = (socket, connectedClients, rooms) => {
   socket.on("join_room", (data) => {
     // Make sure room that user is connecting to does not have more than two clients.
     const activeRooms = getActiveRooms(rooms);
 
-    if (activeRooms.hasOwnProperty(data.room)) {
-      if (activeRooms[data.room].size === 2) {
-        socket.emit("join_room_error", "This room is full.");
+    if (
+      activeRooms.hasOwnProperty(data.room) &&
+      activeRooms[data.room].size === 2
+    ) {
+      socket.emit("join_room_error", "This room is full.");
 
-        console.log(`Room ${data.room} is full.`);
-      } else {
-        joinRoom(socket, connectedClients, data);
-      }
+      console.log(`Room ${data.room} is full.`);
     } else {
-      joinRoom(socket, connectedClients, data);
+      socket.join(data.room);
+      connectedClients[socket.id] = data.user;
+      socket.emit("join_room_success", { room: data.room });
+
+      console.log(
+        `User ${data.user} with ID ${socket.id} connected to room ${data.room}`
+      );
     }
   });
 };
