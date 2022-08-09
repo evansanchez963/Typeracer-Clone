e32a6380828d2950b4609a07979f70d7e562927b
Frost274 <sancheev@sonoma.edu>
2022-08-09 11:04:27 -0700

    Refactor sendTextHandler function

--- server/socketHandlers/sendTextHandler.js
+++ server/socketHandlers/sendTextHandler.js
@@ -1,6 +1,8 @@
 const sendTextHandler = (socket) => {
   socket.on("send_text_data", (data) => {
-    socket.to(data.room).emit("recieve_text_data", { text: data.text });
+    socket
+      .to(data.room)
+      .emit("recieve_text_data", { chars: data.chars, words: data.words });
   });
 };
 
