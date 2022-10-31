18379602b098161d3bddbf86924e52736f976c0e
Evan <sancheev@sonoma.edu>
2022-10-31 14:43:04 -0700

    Change setupProxy.js for deployment

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,20 @@ module.exports = (app) => {
   app.use(
     "/api",
     createProxyMiddleware({
+      target: "https://typeracer-clone-frontend.onrender.com",
+      changeOrigin: true,
+    })
+  );
+};
+
+/*
+module.exports = (app) => {
+  app.use(
+    "/api",
+    createProxyMiddleware({
       target: "http://localhost:5000",
       changeOrigin: true,
     })
   );
 };
+*/
