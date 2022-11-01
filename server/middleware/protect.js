eaf60c7f3672f997b21dcb60713d782c3853d9bc
Evan <sancheev@sonoma.edu>
2022-10-31 18:05:34 -0700

    Code cleanup

--- server/middleware/protect.js
+++ server/middleware/protect.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 const errorResponse = require("../utils/errorResponse");
 
 exports.protect = async (req, res, next) => {
