c4f82679aae7c0aea345e129ce983ebc2dca35a7
Frost274 <sancheev@sonoma.edu>
2022-12-19 23:16:59 -0800

    Change name of models

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-const TypingSession = require("../models/typingSession");
+const TypingSession = require("../models/TypingSession");
 const errorResponse = require("../utils/errorResponse");
 
 // Get user's username and email.
