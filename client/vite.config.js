668b35fb392a17535dbb7bc6b3aa6407d47de030
Frost274 <sancheev@sonoma.edu>
2023-05-04 13:35:22 -0700

    Install and set up Vitest and React Testing Library

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig(() => {
   return {
+    server: {
+      open: true,
+    },
     build: {
       outDir: "build",
     },
-    plugins: [react()],
+    plugins: [react(), svgr({ svgrOptions: { icon: true } })],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./tests/setup.js",
+    },
   };
 });
